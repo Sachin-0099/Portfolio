9857c42c2cfb9565cefb23f0029a88e54c9141e6
Sachin-0099 <itssachin0099@gmail.com>
2025-01-12 11:20:45 +0530

    error resolved in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import About from './components/About'
 import Experience from './components/Experience'
 import Contact from './components/Contact'
 import Nav from './components/Nav'
-import Portfolio from './components/portfolio'
+import Portfolio from "./components/Portfolio";
+
 import Skills from './components/Skills'
 
 
