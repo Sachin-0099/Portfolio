960c06227b687ec7c4bc29a139524e75a5497a84
Muhafiz Raza <muhafizraza1209@gmail.com>
2025-02-10 12:29:11 +0530

    fix links to respective div blocks.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -67,8 +67,7 @@ const Experience = () => {
        
     ]
   return (
-    <div name='experince' 
-    className='bg-gradient-to-b from-gray-800 to to-black w-full h-screen'>
+    <div name='experience' className='bg-gradient-to-b from-gray-800 to to-black w-full h-screen pt-28'>
       
       <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
         <div>
